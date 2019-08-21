e64cecbd309049be454d56cf96b2521d8f0bb9e7
Will Kearney <wkearn@gmail.com>
2019-08-21 14:03:34 -0400

    Add a comparison to the Kalman filter gradient
    As we increase the number of particles, on average,
    the score estimate will get closer to the true score,
    which we can compute using automatic differentiation
    on the Kalman filter

--- test/particle/score.jl
+++ test/particle/score.jl
@@ -1,5 +1,7 @@
 # Tests for score filter
 
+using ForwardDiff
+
 δ = 1.0
 φ = 0.95
 ση = 1.0
@@ -9,15 +11,22 @@
 
 β = [0.0;1.0]
 
-N = 1000
+N = 100
 
 @testset "Score filter" begin
     arh = ARH(β)
 
     X,Y = Filtering.simulate(arh,θ0,N,fill([1.0],N))
 
-    sf = score_filter(arh,θ0,Y,100,fill([1.0],N))
+    S = ForwardDiff.gradient(θ->-filter_likelihood(kalman_filter(arh,θ,Y,fill([1.0],N))),θ0)
 
-    @test loglikelihood(sf) < 0.0
-    @test size(score(sf),1) == length(θ0)    
+    sf1 = score_filter(arh,θ0,Y,100,fill([1.0],N))
+    sf2 = score_filter(arh,θ0,Y,1000,fill([1.0],N))
+    sf3 = score_filter(arh,θ0,Y,10000,fill([1.0],N))
+
+    @test loglikelihood(sf1) < 0.0
+    @test size(score(sf1),1) == length(θ0)
+
+    # This might not always succeed, but should be true
+    @test mean(abs2,S .- score(sf1)) > mean(abs2,S .- score(sf2)) > mean(abs2,S .- score(sf3))
 end
